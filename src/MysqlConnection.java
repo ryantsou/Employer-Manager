4874acb5402c56dec516731e27a61684a6ff5b68
baibsFar <fararano.kevin@gmail.com>
2022-07-24 21:36:26 +0300

    adding comment as setup

--- src/MysqlConnection.java
+++ src/MysqlConnection.java
@@ -9,8 +9,10 @@ public class MysqlConnection {
 		user = "root";
 		pwd = "root";
 		try {
-			Class.forName("org.mariadb.jdbc.Driver"); 
-			Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/empdb?user=root&password=root");
+			// Class.forName("com.mysql.jdbc.Driver");	//	MYSQL server
+			Class.forName("org.mariadb.jdbc.Driver");	//	MARIADB server
+			// Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/DB?user=USER&password=PASSWORD"); //	MYSQL server
+			Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/empdb?user=root&password=root");	//	MARIADB server
 			System.out.println("Connected to MariaDB database");
 			return conn;
 		} catch (Exception e) {
